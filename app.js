cbecb14cc22368a05a41bbe50840eed4bcfedde8
Maciej Wojtasiak <maciekwojtasiak@gmail.com>
2020-08-05 18:38:22 +0200

    add button in header added

--- app.js
+++ app.js
@@ -1,40 +1,53 @@
-function library() {
-    let myLibrary = [];
+let myLibrary = [];
 
-    function Book(title, author, pages) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-    }
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
 
 
-    function addBookToLibrary() {
-        let title = prompt('Book title: ');
-        let author = prompt('Author name: ');
-        let pages = prompt('Number of book pages: ');
+function addBookToLibrary() {
+    let title = prompt('Book title: ');
+    let author = prompt('Author name: ');
+    let pages = prompt('Number of book pages: ');
 
-        let book = new Book(title, author, pages);
-        myLibrary.push(book);
-
-        displayBook(book);
-    }
-
-    function displayBook(book) {
-        const container = document.querySelector('.container');
-        const card = document.createElement('div');
-        card.classList.add('card');
-
-        card.innerHTML =
-            `<span class="book__img"></span>
-    <p class="book__author">Author: ${book.author}</p>
-    <p class="book__title">Title: ${book.title}</p>  
-    <p class="book__pages">No. of pages: ${book.pages}</p>`;
-        container.appendChild(card);
-    }
-
-    const addBtn = document.querySelector('.buttons__btn--add');
-    addBtn.addEventListener('click', addBookToLibrary);
+    let book = new Book(title, author, pages);
+    myLibrary.push(book);
 
 }
 
-document.addEventListener('DOMContentLoaded', library)
+
+
+// function displayBook(book) {
+//     const card = document.createElement('div');
+//     card.classList.add('card');
+
+//     card.innerHTML =
+//         `<span class="book__img"></span>
+//     <p class="book__author">Author: ${book.author}</p>
+//     <p class="book__title">Title: ${book.title}</p>  
+//     <p class="book__pages">No. of pages: ${book.pages}</p>`;
+
+//     const container = document.querySelector('.container');
+//     container.appendChild(card);
+// }
+
+const addBtn = document.querySelector('.header__addBook');
+addBtn.addEventListener('click', addBookToLibrary);
+
+
+
+// const card = document.createElement('div');
+// const container = document.querySelector('containter');
+// container.appendChild(card);
+// card.classList.add('card')
+// card.innerHTML =
+//     `<span class="book__img"></span>
+//             <p class="book__author">Author:</p>
+//             <p class="book__title">Title: </p>  
+//             <p class="book__pages">No. of pages:</p>`;
+
+
+
+
